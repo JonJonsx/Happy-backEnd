3ce084172f096ceb401b865b88a2cedcc576c423
JonJonsx <jonatan.souza@bandtec.com.br>
2020-10-16 17:07:46 -0300

    Lidando com exceções

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -1,15 +1,18 @@
 import { Request, Response} from 'express';
-
 import { getRepository } from 'typeorm';
+import orphanageView from '../views/orphanages_view';
+
 import Orphanage from '../models/Orphanage';
 
 export default {
   async index(request: Request, response: Response){
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanages = await orphanagesRepository.find();
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images']
+    });
 
-    return response.json(orphanages);
+    return response.json(orphanageView.renderMany(orphanages));
   },
   
   async show(request: Request, response: Response){
@@ -17,9 +20,11 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanage = await orphanagesRepository.findOneOrFail(id);
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ['images']
+    });
 
-    return response.json(orphanage);
+    return response.json(orphanageView.render(orphanage));
   },
 
   async create(request: Request, response: Response) {
@@ -35,6 +40,11 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanage);
 
+    const requestImages = request.files as Express.Multer.File[];
+    const images = requestImages.map(image => {
+      return { path: image.filename}
+    })
+
     const orphanage = orphanagesRepository.create({
       name,
       latitude,
@@ -43,10 +53,10 @@ export default {
       instructions,
       opening_hours,
       open_on_weekends,
+      images,
     });
 
     await orphanagesRepository.save(orphanage)
-
 
     return response.status(201).json(orphanage);
   }

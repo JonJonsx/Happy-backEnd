8696a422147ef452d812df0ea0e6724ae31c6dac
JonJonsx <jonatan.souza@bandtec.com.br>
2020-10-16 11:37:20 -0300

    Inserção sem imagem funcionando

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
+
 
 import './database/connection';
 
@@ -6,8 +9,32 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/users/:id',(request,response) =>{
-    return response.json({mensagem: 'hello world'});
+app.post('/orphanages', async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+
+  await orphanagesRepository.save(orphanage)
+
+  return response.json({ mensagem: 'hello world' });
 })
 
 app.listen(3333);
